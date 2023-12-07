4a94f5aa449ff5f6fa9a0d83fc4e3260d707eb25
Vadim Brodsky <vadim.brodsky@coinbase.com>
2023-12-07 07:27:51 -0500

    WIP

--- 2023/go/day_03/day_03.go
+++ 2023/go/day_03/day_03.go
@@ -1,12 +1,79 @@
 package day03
 
-func GetParts(input string) []int {
-  return []int{467, 35, 633, 617, 592, 755, 664, 598}
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+type Schematic struct {
+	input string
+	lines []string
+}
+
+type Part struct {
+	value string
+	x1    int
+	x2    int
+	y     int
+}
+
+type Symbol struct {
+	x     int
+	y     int
+	value string
+}
+
+func GetParts(input string) (usedParts []int) {
+	schematic := NewSchematic(input)
+	parts, symbols := schematic.Parse(".")
+
+	fmt.Printf("parts: %v\n", parts)
+	fmt.Printf("symbols: %v\n", symbols)
+
+	return
 }
 
 func Sum(a []int) (sum int) {
-  for _, e := range a {
-    sum += e
-  }
-  return
+	for _, e := range a {
+		sum += e
+	}
+	return
+}
+
+func NewSchematic(input string) Schematic {
+	return Schematic{
+		input: input,
+		lines: strings.Split(input, "\n"),
+	}
+}
+
+func (s Schematic) Parse(del string) (parts []Part, symbols []Symbol) {
+	for x, line := range s.lines {
+		// fmt.Printf("line %d: %s \n", i, line)
+
+		for y, char := range line {
+			// fmt.Printf("%s", string(char))
+
+			if string(char) != del && !unicode.IsNumber(char) {
+				symbol := Symbol{x: x, y: y, value: string(char)}
+				symbols = append(symbols, symbol)
+			}
+
+			numberPatter := regexp.MustCompile(`\d+`)
+			matches := numberPatter.FindIndex([]byte(line))
+
+			fmt.Printf("matches: %v\n", matches)
+
+			if unicode.IsNumber(char) {
+				part := Part{value: string(char), x1: x, x2: x, y: y}
+				parts = append(parts, part)
+			}
+		}
+
+		fmt.Printf("\n")
+	}
+
+	return
 }
