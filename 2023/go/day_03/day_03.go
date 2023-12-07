fe2a0d40ddfc102af45089e703cf526973c5b33b
Vadim Brodsky <vadim.brodsky@coinbase.com>
2023-12-07 06:31:37 -0500

    start on day 3

---
+++ 2023/go/day_03/day_03.go
@@ -0,0 +1,12 @@
+package day03
+
+func GetParts(input string) []int {
+  return []int{467, 35, 633, 617, 592, 755, 664, 598}
+}
+
+func Sum(a []int) (sum int) {
+  for _, e := range a {
+    sum += e
+  }
+  return
+}
---
+++ 2023/go/day_03/day_03_test.go
@@ -0,0 +1,36 @@
+package day03
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestDay03(t *testing.T) {
+  sampleSchematic := `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`
+
+  t.Run("get the correct sum for engine part numbers for the sample output", func(t *testing.T) {
+    expectedPartNumbers := []int{467, 35, 633, 617, 592, 755, 664, 598}
+    expectedSum := 4361
+
+    gotPartNumbers := GetParts(sampleSchematic)
+    gotSum := Sum(gotPartNumbers)
+
+    if !slices.Equal(expectedPartNumbers, gotPartNumbers) {
+      t.Errorf("expected %v, got %v", expectedPartNumbers, gotPartNumbers)
+    }
+
+    if expectedSum != gotSum {
+      t.Errorf("expected sum %d, got %d", expectedSum, gotSum)
+    }
+  })
+  
+}
