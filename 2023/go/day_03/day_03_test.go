3b6ae374cf556cee90bcff3d02c5faaa6c13a2b5
Vadim Brodsky <vadim.brodsky@gmail.com>
2023-12-08 07:14:42 -0500

    Go format

--- 2023/go/day_03/day_03_test.go
+++ 2023/go/day_03/day_03_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestDay03(t *testing.T) {
-  sampleSchematic := `467..114..
+	sampleSchematic := `467..114..
 ...*......
 ..35..633.
 ......#...
@@ -17,20 +17,20 @@ func TestDay03(t *testing.T) {
 ...$.*....
 .664.598..`
 
-  t.Run("get the correct sum for engine part numbers for the sample output", func(t *testing.T) {
-    expectedPartNumbers := []int{467, 35, 633, 617, 592, 755, 664, 598}
-    expectedSum := 4361
+	t.Run("get the correct sum for engine part numbers for the sample output", func(t *testing.T) {
+		expectedPartNumbers := []int{467, 35, 633, 617, 592, 755, 664, 598}
+		expectedSum := 4361
 
-    gotPartNumbers := GetParts(sampleSchematic)
-    gotSum := Sum(gotPartNumbers)
+		gotPartNumbers := GetParts(sampleSchematic)
+		gotSum := Sum(gotPartNumbers)
 
-    if !slices.Equal(expectedPartNumbers, gotPartNumbers) {
-      t.Errorf("expected %v, got %v", expectedPartNumbers, gotPartNumbers)
-    }
+		if !slices.Equal(expectedPartNumbers, gotPartNumbers) {
+			t.Errorf("expected %v, got %v", expectedPartNumbers, gotPartNumbers)
+		}
 
-    if expectedSum != gotSum {
-      t.Errorf("expected sum %d, got %d", expectedSum, gotSum)
-    }
-  })
-  
+		if expectedSum != gotSum {
+			t.Errorf("expected sum %d, got %d", expectedSum, gotSum)
+		}
+	})
+
 }
