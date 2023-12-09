89625f20455095b4d470f91e874e689e39656861
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2023-12-09 11:33:51 -0500

    wip

--- 2023/go/day_04/day_04.go
+++ 2023/go/day_04/day_04.go
@@ -1,5 +1,11 @@
 package day04
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Card struct {
 	numbers        []int
 	winningNumbers []int
@@ -8,20 +14,54 @@ type Card struct {
 type Cards []Card
 
 func newCards(s string) (cards Cards) {
-  return
+	cardsStrings := strings.Split(s, "\n")
+	for _, c := range cardsStrings {
+		_, numbers, _ := strings.Cut(c, ": ")
+		n, wn, _ := strings.Cut(numbers, "|")
+
+		cards = append(cards, Card{
+			numbers:        parseNumbers(n),
+			winningNumbers: parseNumbers(wn),
+		})
+	}
+
+	fmt.Printf("Cards: %v\n", cards)
+	return
 }
 
-func (c Cards) GetWinningNumbers() (winningNumbers [][]int ){
-  return
+func (cards Cards) GetWinningNumbers() (winningNumbers [][]int) {
+	for _, card := range cards {
+		var winningNumbersForCard []int
+		for _, wn := range card.winningNumbers {
+			for _, n := range card.numbers {
+				if n == wn {
+					winningNumbersForCard = append(winningNumbersForCard, wn)
+				}
+			}
+		}
+		winningNumbers = append(winningNumbers, winningNumbersForCard)
+	}
+	return
 }
 
 func (c Cards) GetPoints() (points []int) {
-  return
+	return
 }
 
 func (c Cards) GetScore() (score int) {
-  return
+	return
 }
 
+func parseNumbers(numberString string) (numbers []int) {
+	numberSlice := strings.Split(numberString, " ")
+	for _, s := range numberSlice {
 
+		parsedInt, err := strconv.ParseInt(s, 10, 32)
+		if err != nil {
+			numbers = append(numbers, 0)
+		}
+		numbers = append(numbers, int(parsedInt))
+	}
 
+	return
+}
