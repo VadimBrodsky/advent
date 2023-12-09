120c7d18636c7d27d7ec46b8c3d8c0a5daadad12
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2023-12-09 10:57:05 -0500

    start on day 4

---
+++ 2023/go/day_04/day_04.go
@@ -0,0 +1,27 @@
+package day04
+
+type Card struct {
+	numbers        []int
+	winningNumbers []int
+}
+
+type Cards []Card
+
+func newCards(s string) (cards Cards) {
+  return
+}
+
+func (c Cards) GetWinningNumbers() (winningNumbers [][]int ){
+  return
+}
+
+func (c Cards) GetPoints() (points []int) {
+  return
+}
+
+func (c Cards) GetScore() (score int) {
+  return
+}
+
+
+
