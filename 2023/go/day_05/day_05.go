aa7da6b8881be7751e77955a6856111b2b8b1141
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2023-12-12 15:56:24 -0500

    wip

--- 2023/go/day_05/day_05.go
+++ 2023/go/day_05/day_05.go
@@ -2,6 +2,7 @@ package day05
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -14,6 +15,36 @@ type Mapping struct {
 	temperature int
 	humidity    int
 	location    int
+}
+
+type Relation struct {
+	from   string
+	to     string
+	length string
+}
+
+type Relations []Relation
+
+type Almanac struct {
+	seeds                 []string
+	seedToSoil            Relations
+	soilToFertilizer      Relations
+	fertilizerToWater     Relations
+	waterToLight          Relations
+	lightToTemperature    Relations
+	temperatureToHumidity Relations
+	humidityToLocation    Relations
+}
+
+var Categoies = []string{
+	"seeds",
+	"seed-to-soil",
+	"soil-to-fertilizer",
+	"fertilizer-to-water",
+	"water-to-light",
+	"light-to-temperature",
+	"temperature-to-humidity",
+	"humidity-to-location",
 }
 
 type Mappings []Mapping
@@ -29,6 +60,20 @@ func (m Mappings) String() (s string) {
 	return
 }
 
+func (a Almanac) String() (s string) {
+	return fmt.Sprintf(`{
+seeds: %q,
+seedToSoil: %v,
+soilToFertilizer: %v,
+fertilizerToWater: %v,
+waterToLight %v,
+lightToTemperature: %v,
+temperatureToHumidity: %v,
+humidityToLocation: %v,
+}`, a.seeds, a.seedToSoil, a.soilToFertilizer, a.fertilizerToWater, a.waterToLight, a.lightToTemperature, a.temperatureToHumidity, a.humidityToLocation)
+
+}
+
 func NewAlmanac(input string) (m Mapping) {
 	replacements := map[string]string{
 		"\n":   " ",
@@ -40,13 +85,51 @@ func NewAlmanac(input string) (m Mapping) {
 		" h":   "\nh",
 		" l":   "\nl",
 	}
-  for old, new := range replacements {
-    input = strings.ReplaceAll(input, old, new)
-  }
+	for old, new := range replacements {
+		input = strings.ReplaceAll(input, old, new)
+	}
 
-	parts := strings.Split(input, "\n")
-	fmt.Printf("%q", parts)
+	lines := strings.Split(input, "\n")
+	var al Almanac
+	re := regexp.MustCompile(`\d+`)
+	for _, line := range lines {
+		l := strings.Split(line, ":")
+
+		switch {
+		case strings.Contains(l[0], "seeds"):
+			al.seeds = re.FindAllString(l[1], -1)
+		case strings.Contains(l[0], "seed-to-soil"):
+			al.seedToSoil.append(re.FindAllString(l[1], -1))
+		case strings.Contains(l[0], "soil-to-fertilizer"):
+			al.soilToFertilizer.append(re.FindAllString(l[1], -1))
+		case strings.Contains(l[0], "fertilizer-to-water"):
+			al.fertilizerToWater.append(re.FindAllString(l[1], -1))
+		case strings.Contains(l[0], "water-to-light"):
+			al.waterToLight.append(re.FindAllString(l[1], -1))
+		case strings.Contains(l[0], "light-to-temperature"):
+			al.lightToTemperature.append(re.FindAllString(l[1], -1))
+		case strings.Contains(l[0], "temperature-to-humidity"):
+			al.temperatureToHumidity.append(re.FindAllString(l[1], -1))
+		case strings.Contains(l[0], "humidity-to-location"):
+			al.humidityToLocation.append(re.FindAllString(l[1], -1))
+		}
+
+		fmt.Printf("%q", line)
+	}
+
+	fmt.Printf("\n%v", al)
 	return
+}
+
+func (r *Relations) append(values []string) {
+	for i := 0; i < len(values); i += 3 {
+		*r = append(*r, Relation{
+			to:     values[i],
+			from:   values[i+1],
+			length: values[i+2],
+		})
+
+	}
 }
 
 func (m Mapping) GetMappings() (mappings Mappings) {
