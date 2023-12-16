0333b105bda45552b71d29299341556ec5736599
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2023-12-15 20:36:07 -0500

    Complete part 2

--- 2023/go/day_06/day_06_test.go
+++ 2023/go/day_06/day_06_test.go
@@ -3,6 +3,7 @@ package day06
 import (
 	"os"
 	"slices"
+	"strings"
 	"testing"
 )
 
@@ -10,7 +11,7 @@ func TestDay06(t *testing.T) {
 	sampleRaces := `Time:      7  15   30
 Distance:  9  40  200`
 
-	t.Run("get the correct value to beat the race for the sample input", func(t *testing.T) {
+	t.Run("get the correct value to beat the races for the sample input", func(t *testing.T) {
 		wantWaysToWin := []int{4, 8, 9}
 		wantWinProduct := 288
 
@@ -27,7 +28,7 @@ Distance:  9  40  200`
 		}
 	})
 
-	t.Run("get the correct value to beat the race for the full input", func(t *testing.T) {
+	t.Run("get the correct value to beat the races for the full input", func(t *testing.T) {
 		fullRaceInput, err := os.ReadFile("input.txt")
 		if err != nil {
 			t.Fatal(err)
@@ -36,6 +37,35 @@ Distance:  9  40  200`
 		wantWinProduct := 275724
 
 		races := NewBoatRaces(string(fullRaceInput))
+		gotWinProduct := races.WinsProduct()
+
+		if wantWinProduct != gotWinProduct {
+			t.Errorf("want %v, got %v", wantWinProduct, gotWinProduct)
+		}
+	})
+
+	t.Run("get the correct value to beat the single race for the sample input", func(t *testing.T) {
+		wantWinProduct := 71503
+
+		sampleRacesWithoutSpaces := strings.ReplaceAll(sampleRaces, " ", "")
+		races := NewBoatRaces(sampleRacesWithoutSpaces)
+		gotWinProduct := races.WinsProduct()
+
+		if wantWinProduct != gotWinProduct {
+			t.Errorf("want %v, got %v", wantWinProduct, gotWinProduct)
+		}
+	})
+
+	t.Run("get the correct value to beat the single race for the full input", func(t *testing.T) {
+		fullRaceInput, err := os.ReadFile("input.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		wantWinProduct := 37286485
+
+		fullRacesWithoutSpaces := strings.ReplaceAll(string(fullRaceInput), " ", "")
+		races := NewBoatRaces(string(fullRacesWithoutSpaces))
 		gotWinProduct := races.WinsProduct()
 
 		if wantWinProduct != gotWinProduct {
