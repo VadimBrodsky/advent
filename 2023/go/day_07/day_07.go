3c069b1e7ffbbfdf96c790c9320a22513bcfbfea
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2023-12-16 16:25:54 -0500

    Add rules

--- 2023/go/day_07/day_07.go
+++ 2023/go/day_07/day_07.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -24,6 +26,8 @@ func NewCamelGame(input string) (game Hands) {
 	}
 
 	fmt.Printf("game: %v\n", game)
+	m := game[0].handToMap()
+	fmt.Printf("mapp: %v, len: %d\n", m, len(m))
 	return
 }
 
@@ -56,4 +60,85 @@ func (h Hands) ParseHandsAndBids(reader *bufio.Scanner) (game Hands, err error) 
 
 func (h Hand) String() string {
 	return fmt.Sprintf("{ cards: %v, bid: %d }", h.cards, h.bid)
+}
+
+func (h Hand) handToMap() (handMap map[string]int) {
+	re := regexp.MustCompile(`[AKQJT98765432]`)
+	matches := re.FindAllString(h.cards, -1)
+	handMap = make(map[string]int, len(matches))
+
+	for _, match := range matches {
+		handMap[match]++
+	}
+
+	return handMap
+}
+
+// where all five cards have the same label: AAAAA
+func (h Hand) IsFiveOfAKind() (result bool) {
+	return len(h.handToMap()) == 1
+}
+
+// where four cards have the same label and one card has a different label: AA8AA
+func (h Hand) IsFourOfAKind() (result bool) {
+	values := getSortedValues(h.handToMap())
+
+	if slices.Equal(values, []int{1, 4}) {
+		result = true
+	}
+	return
+}
+
+// where three cards have the same label, and the remaining two cards share a different label: 23332
+func (h Hand) IsFullHouse() (result bool) {
+	values := getSortedValues(h.handToMap())
+
+	if slices.Equal(values, []int{2, 3}) {
+		result = true
+	}
+	return
+}
+
+// where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+func (h Hand) IsThreeOfAKind() (result bool) {
+	values := getSortedValues(h.handToMap())
+
+	if slices.Equal(values, []int{1, 1, 3}) {
+		result = true
+	}
+	return
+}
+
+// where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+func (h Hand) IsTwoPair() (result bool) {
+	values := getSortedValues(h.handToMap())
+
+	if slices.Equal(values, []int{1, 2, 2}) {
+		result = true
+	}
+	return
+}
+
+// where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+func (h Hand) IsOnePair() (result bool) {
+	values := getSortedValues(h.handToMap())
+
+	if slices.Equal(values, []int{1, 1, 1, 2}) {
+		result = true
+	}
+	return
+}
+
+// where all cards' labels are distinct: 23456
+func (h Hand) IsHighCard() (result bool) {
+	return len(h.handToMap()) == 5
+}
+
+func getSortedValues(m map[string]int) (values []int) {
+	for _, v := range m {
+		values = append(values, v)
+	}
+
+	slices.Sort(values)
+	return
 }
